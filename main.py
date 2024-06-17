265adeb3535059c69656fa24d204e0348ce0f141
AsaelCG <asael.cocas@hotmail.com>
2024-06-17 02:34:25 -0600

    ya lograda el ingreso de página

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route ("/")
-def hello():
+def index():
     return "Hola putos"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888)
