9c0bd1104a4c27061af16abb09187abb4ac8e9a7
AsaelCG <asael.cocas@hotmail.com>
2024-06-18 02:53:38 -0600

    creación de carpeta de templates y archivos html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route ("/")
 def index():
-    return "Hola putos"
+    return render_template('index.html')
+
+@app.route ("/estudiantes")
+def estudiantes():
+    return render_template('estudiantes.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
