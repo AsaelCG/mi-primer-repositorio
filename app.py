2ee71fad62b219b33603bd0efaade2d5230726de
AsaelCG <asael.cocas@hotmail.com>
2024-06-18 14:49:04 -0600

    creación e implementación de página de materias

--- app.py
+++ app.py
@@ -15,5 +15,15 @@ def estudiantes():
     ]
     return render_template('estudiantes.html', data=data)
 
+@app.route("/materias")
+def materias ():
+    materias = [
+        {"nombre":"Analisis y Diseños de Algoritmos", "calificacion": 10},
+        {"nombre":"Programación Orientada a Objetos", "calificacion": 9},
+        {"nombre":"Bases de datos", "calificacion": 7},
+        {"nombre":"Programación Avanzada", "calificacion": 10}
+    ]
+    return render_template ('materias.html', materias = materias)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
