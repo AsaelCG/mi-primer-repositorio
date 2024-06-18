e6abb6289226164c474006c8ee37865c9fcc7198
AsaelCG <asael.cocas@hotmail.com>
2024-06-18 14:33:50 -0600

    actualización rápida del template estudiantes y app,py

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ def index():
 
 @app.route ("/estudiantes")
 def estudiantes():
-    return render_template('estudiantes.html')
+    data = [
+        {"matricula" : "230702", "nombre": "Asael", "apellido_paterno": "Cisneros", "apellido_materno" : "Gonzalez"},
+        {"matricula" : "230678", "nombre": "Jamila", "apellido_paterno": "Cisneros", "apellido_materno" : "Gonzalez"},
+        {"matricula" : "230973", "nombre": "Vanessa", "apellido_paterno": "Villalobos", "apellido_materno" : "Villalobos"}
+    ]
+    return render_template('estudiantes.html', data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
